e5e49a9b814ac3d6610d377edf915e11f75228ef
Jonathan <jalmeida0291@gmail.com>
2018-08-19 20:00:05 -0400

    first draft of including local storage

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ const form = document.querySelector("#form");
 const input = document.querySelector("#input");
 const ol = document.querySelector("ol");
 
+// ** Need to implement cross though logic... if user closes session
+
+// Check if local storage exists in users browser return an empty array if otherwise
+let todos = localStorage.getItem("todos");
+if (!todos) {
+  todos = [];
+  // If todos is found in localStorage, display in new session
+} else {
+  displaylocalStorageTodos();
+}
+
 // DOM TRANSVERSAL WITH TEXT CONTENT IS BETTER WAY ACCEPT USER INPUT
 form.addEventListener("submit", function(e) {
   if (input.value) {
-    let li = document.createElement("li");
-    let button = document.createElement("button");
-    let content = document.createTextNode(`${input.value}`);
-    button.textContent = "Delete";
-    button.className = "hidden";
-    li.appendChild(button);
-    li.appendChild(content);
-    ol.appendChild(li);
+    if (typeof todos === "string") {
+      todos = JSON.parse(todos);
+    }
+    // If a completed is true for a todo, display line through class
+    todos.push({
+      value: input.value,
+      completed: false
+    });
+    // Need to reset local storage everytime todos updates...
+    localStorage.setItem("todos", JSON.stringify(todos));
+    generateHTMLNodes(input.value);
     input.value = "";
   }
   e.preventDefault();
@@ -27,17 +41,27 @@ ol.addEventListener("click", function(e) {
 ol.addEventListener("click", function(e) {
   if (e.target.nodeName === "BUTTON") {
     let li = e.target.parentNode;
+    //
+    const todoIndex = Array.from(ol.children).indexOf(li);
+    // console.log(todoIndex);
+    //
+    if (typeof todos === "string") {
+      todos = JSON.parse(todos);
+      todos.splice(todoIndex, 1);
+      todos = JSON.stringify(todos);
+    } else {
+      todos.splice(todoIndex, 1);
+      todos = JSON.stringify(todos);
+    }
+    // Need to reset local storage everytime todos updates...
+    localStorage.setItem("todos", todos);
     ol.removeChild(li);
   }
 });
 
-ol.addEventListener("mouseover", function(e) {
-  showDeleteButton(e);
-});
+ol.addEventListener("mouseover", showDeleteButton);
 
-ol.addEventListener("mouseout", function(e) {
-  hideDeleteButton(e);
-});
+ol.addEventListener("mouseout", hideDeleteButton);
 
 function showDeleteButton(e) {
   // console.log(e.target.firstChild);
@@ -56,81 +80,22 @@ function hideDeleteButton(e) {
   }
 }
 
-// VERSION 1 without using Form:
+function displaylocalStorageTodos() {
+  if (typeof todos === "string") {
+    JSON.parse(todos).forEach(function(todo) {
+      // Passing in todo.value rather than just todo since each todo is an object
+      generateHTMLNodes(todo.value);
+    });
+  }
+}
 
-// const input = document.querySelector("#input");
-// const ol = document.querySelector("ol");
-
-// // DOM TRANSVERSAL WITH TEXT CONTENT IS BETTER WAY ACCEPT USER INPUT
-// input.addEventListener("keydown", function(e) {
-//   if (e.keyCode === 13 && input.value) {
-//     let li = document.createElement("li");
-//     let button = document.createElement("button");
-//     let content = document.createTextNode(`${input.value}`);
-//     button.textContent = "Delete";
-//     button.className = "hidden";
-//     li.appendChild(button);
-//     li.appendChild(content);
-//     ol.appendChild(li);
-//     input.value = "";
-//   }
-// });
-
-// ol.addEventListener("click", function(e) {
-//   if (e.target.nodeName === "BUTTON") {
-//     let li = e.target.parentNode;
-//     ol.removeChild(li);
-//   }
-// });
-
-// ol.addEventListener("mouseover", function(e) {
-//   showDeleteButton(e);
-// });
-
-// ol.addEventListener("mouseout", function(e) {
-//   hideDeleteButton(e);
-// });
-
-// ol.addEventListener("click", function(e) {
-//   if (e.target.nodeName === "LI" && e.target.className === "") {
-//     e.target.className = "done-todo";
-//   } else {
-//     e.target.className = "";
-//   }
-// });
-
-// function showDeleteButton(e) {
-//   if (e.target.nodeName === "LI") {
-//     e.target.firstChild.className = "";
-//   } else if (e.target.nodeName === "BUTTON") {
-//     e.target.className = "";
-//   }
-// }
-
-// function hideDeleteButton(e) {
-//   if (e.target.nodeName === "LI") {
-//     e.target.firstChild.className = "hidden";
-//   } else if (e.target.nodeName === "BUTTON") {
-//     e.target.className = "hidden";
-//   }
-// }
-
-// THIS WILL ALLOW THE USER TO INPUT SCRIPT TAGS WHICH CAN CAUSE PROBLEMS TO THIS PROGRAM.
-// input.addEventListener("keydown", function (e) {
-//     if (e.keyCode === 13 && input.value) {
-//         let li = document.createElement("li");
-//         li.innerHTML = `<button class="hidden">Delete</button> <span>${this.value}</span>`;
-//         ol.appendChild(li);
-//         input.value = "";
-//     }
-// });
-
-// THIS WILL ALLOW THE USER TO INPUT SCRIPT TAGS WHICH CAN CAUSE PROBLEMS TO THIS PROGRAM.
-// input.addEventListener("keydown", function (e) {
-//     if (e.keyCode === 13 && input.value) {
-//         let li = document.createElement("li");
-//         li.innerHTML = `<button class="hidden">Delete</button> <span>${this.value}</span>`;
-//         ol.appendChild(li);
-//         input.value = "";
-//     }
-// });
+function generateHTMLNodes(el) {
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  const content = document.createTextNode(`${el}`);
+  button.textContent = "Delete";
+  button.className = "hidden";
+  li.appendChild(button);
+  li.appendChild(content);
+  ol.appendChild(li);
+}
